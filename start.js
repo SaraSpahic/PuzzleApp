1b0644cd7e9d3eb5b06282f33c3be79287cdf18b
Sara Spahic <sara_spahic@yahoo.com>
2018-10-18 18:35:43 +0200

    Connecting to database

--- start.js
+++ start.js
@@ -1,6 +1,20 @@
-const app = require('./app');
-const port=process.env.PORT || 3000;
+require('dotenv').config();
+const mongoose = require('mongoose');
 
-const server = app.listen(port, () => {
+mongoose.connect(process.env.DATABASE);
+mongoose.Promise = global.Promise;
+mongoose.connection
+  .on('connected', () => {
+    console.log(`Mongoose connection open on ${process.env.DATABASE}`);
+  })
+  .on('error', (err) => {
+    console.log(`Connection error: ${err.message}`);
+  });
+
+  require('./models/Registration');
+
+
+const app = require('./app');
+const server = app.listen(3000, () => {
   console.log(`Express is running on port ${server.address().port}`);
 });
