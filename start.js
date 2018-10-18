89dc071ccd44b4d450c9f399d2ebafdb92ae3194
Sara Spahic <sara_spahic@yahoo.com>
2018-10-18 18:50:40 +0200

    Changed port

--- start.js
+++ start.js
@@ -15,6 +15,6 @@ mongoose.connection
 
 
 const app = require('./app');
-const server = app.listen(3000, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Express is running on port ${server.address().port}`);
 });
